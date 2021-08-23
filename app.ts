000c67266b742d77456b3802f1643cd7df98011b
szymonszoldra <szymon.szoldra@gmail.com>
2021-08-23 23:44:41 +0200

    Add mongodb to the project

--- app.ts
+++ app.ts
@@ -1,11 +1,28 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import * as config from './utils/config';
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'dist')));
+
+mongoose.connect(config.MONGODB_URI!, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+}).then(() => {
+  console.log('Connected');
+  const temp = new mongoose.Schema({
+    name: String,
+  });
+  const Cat = mongoose.model('Cat', temp);
+
+  const kitty = new Cat({ name: 'Zildjian' });
+  kitty.save().then(() => console.log('meow'));
+}).catch((e) => console.log(e));
 
 app.get('/', (_req, res): void => {
   res.render('index');
--- utils/config.ts
+++ utils/config.ts
@@ -1,3 +1,7 @@
 require('dotenv').config();
 
 export const { PORT } = process.env;
+
+export const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.MONGO_TEST_URI
+  : process.env.MONGO_DEV_URI;
