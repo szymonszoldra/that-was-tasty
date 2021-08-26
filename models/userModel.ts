fbb76b8ef798e35dfde38703ce749af39a963098
szymonszoldra <szymon.szoldra@gmail.com>
2021-08-26 20:20:32 +0200

    Add user model

---
+++ models/userModel.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+import passportLocalMongoose from 'passport-local-mongoose';
+import isEmail from 'validator/lib/isEmail';
+
+export interface User {
+  email: string,
+  name: string,
+}
+
+export interface UserDocument extends User, mongoose.Document {
+  created: Date,
+  hash: string
+}
+
+const UserSchema = new mongoose.Schema<UserDocument>({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    required: 'You have to supply an email',
+    validate: [isEmail, 'This is not the correct email!'],
+  },
+  name: {
+    type: String,
+    required: 'You have to supply a name',
+    trim: true,
+  },
+});
+
+// @ts-ignore
+UserSchema.plugin(uniqueValidator);
+// @ts-ignore
+UserSchema.plugin(passportLocalMongoose, {
+  usernameField: 'email',
+});
+
+export default mongoose.model<UserDocument>('User', UserSchema);
